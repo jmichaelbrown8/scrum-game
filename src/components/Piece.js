7584dc2699801372a023bfc55b0c2f435341a079
J. Michael Brown <jmichael.brown@nike.com>
2018-11-27 11:51:25 -0800

    Added SVG shapes for pieces to use

--- src/components/Piece.js
+++ src/components/Piece.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Draggable } from 'react-beautiful-dnd';
+import Shape from './Shape';
 
 class Piece extends Component {
   render() {
@@ -7,14 +8,15 @@ class Piece extends Component {
     var index = this.props.index;
 
     return (
-      <Draggable draggableId={piece.id} index={index}>
+      <Draggable draggableId={piece.id} index={index} >
         {(provided) => (
           <div className="Piece"
               {...provided.draggableProps}
               {...provided.dragHandleProps}
               ref={provided.innerRef}
           >
-            {piece.content}
+            <Shape id={piece.id}></Shape>
+            <div>{piece.content}</div>
           </div>
         )}
       </Draggable>
